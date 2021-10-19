098c244a583a4a5ec93a31eadcbef4173718a32e
EvgeniUPS <evgeniups@gmail.com>
2021-10-19 17:00:54 +0300

    fetch

---
+++ #src/js/files/api.js
@@ -0,0 +1,39 @@
+const url = 'https://jsonplaceholder.typicode.com/photos?_limit=5'
+// const url = 'https://new.eliteukrainerating.com/ajax/get_products/10/'
+
+const getData = async () => {
+  let response = await fetch(url)
+
+  if (response.ok) {
+    // если HTTP-статус в диапазоне 200-299
+    // получаем тело ответа (см. про этот метод ниже)
+    let json = await response.json()
+    console.table(`json`, json)
+    renderData(json)
+  } else {
+    alert('Ошибка HTTP: ' + response.status)
+  }
+}
+
+const renderData = data => {
+  const tempQ = document.querySelector('.temp-q')
+
+  data.forEach(element => {
+    const html = `  
+	<div class="temp-q__item">
+    <div class="temp-q__header">name</div>
+    <div class="temp-q__dolgnost">dolgnost</div>
+    <div class="temp-q__image">
+      <img src="https://placeimg.com/640/480/tech" alt="example" />
+    </div>
+  </div>
+`
+    tempQ.insertAdjacentHTML('beforeend', html)
+  })
+  console.log(data)
+}
+const testQBtn = document.querySelector('#testQBtn')
+testQBtn.addEventListener('click', getData)
+
+// getData(url)
+// renderData()
