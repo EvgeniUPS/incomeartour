d61f2defb735c0f96765f1eaea4c4c3d95c6402e
EvgeniUPS <evgeniups@gmail.com>
2021-11-17 11:44:13 +0200

    api

--- #src/js/files/api.js
+++ #src/js/files/api.js
@@ -1,34 +1,64 @@
-const url = 'https://jsonplaceholder.typicode.com/photos?_limit=10'
-// const url = 'https://new.eliteukrainerating.com/ajax/get_products/10/'
+const url = ''
+const reqId = ''
+const perPage = '2'
+const typeData = `country`
 
 const getData = async () => {
+  if (reqId) {
+    url = `http://localhost:3002/${typeData}/${reqId}`
+  } else if (perPage) {
+    url = `http://localhost:3002/${typeData}?_limit=${perPage}`
+  } else {
+    url = `http://localhost:3002/${typeData}`
+  }
+  console.log(url)
+
   let response = await fetch(url)
 
   if (response.ok) {
     // если HTTP-статус в диапазоне 200-299
     // получаем тело ответа (см. про этот метод ниже)
     let json = await response.json()
-
-    renderData(json)
+    // renderData(json)
+    console.log(json)
   } else {
-    alert('Ошибка HTTP: ' + response.status)
+    console.warn('Ошибка HTTP: ' + response.status)
   }
 }
-
-const renderData = data => {
-  const tempQ = document.querySelector('.temp-q')
-  data.forEach(element => {
-    const html = `  
-	<div class="temp-q__item">
-    <div class="temp-q__header">${element.title}</div>
-    <div class="temp-q__dolgnost">dolgnost</div>
-    <div class="temp-q__image">
-      <img src="${element.url}" alt="example" />
-    </div>
-  </div>
-`
-    tempQ.insertAdjacentHTML('beforeend', html)
-  })
+getData()
+let _data = {
+  id: Date.now(),
+  country_name: 'Brazil',
+  country_code: 'BR',
+  city: 'Corrego Grande',
 }
-const testQBtn = document.querySelector('#testQBtn')
+
+function sendData() {
+  fetch('http://localhost:3002/country', {
+    method: 'POST',
+    body: JSON.stringify(_data),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+}
+// sendData()
+
+// const renderData = data => {
+//   const tempQ = document.querySelector('.temp-q')
+//   data.forEach(element => {
+//     const html = `
+// 	<div class="temp-q__item">
+//     <div class="temp-q__header">${element.title}</div>
+//     <div class="temp-q__dolgnost">dolgnost</div>
+//     <div class="temp-q__image">
+//       <img src="${element.url}" alt="example" />
+//     </div>
+//   </div>
+// `
+//     tempQ.insertAdjacentHTML('beforeend', html)
+//   })
+// }
+// const testQBtn = document.querySelector('#testQBtn')
 // testQBtn.addEventListener('click', getData)
